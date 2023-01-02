bbeef45e4e4e718ff8c9e24c39dd9663b8b0207c
AlissonJB13 <alissonjb13@gmail.com>
2023-01-02 20:08:25 -0300

    adicionada a lista, esta retornando somente strings

--- bio_if/lib/home.dart
+++ bio_if/lib/home.dart
@@ -61,50 +61,108 @@ class _HomeState extends State<Home> {
         .get();
 
     for (DocumentSnapshot doc in snapshot.docs) {
-    var item = Postagem.fromDocument(doc);
-    print(item.toString());
-  }
-    
+      var item = Postagem.fromDocument(doc);
+      print(item.toString());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        iconTheme: const IconThemeData(
-          color: Colors.white,
-        ),
-        title: Text("Bio IF"),
-        /*Image.asset(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+          ),
+          title: Text("Bio IF"),
+          /*Image.asset(
           'imagens/logo.png',
           height: 22,
         ),*/
-        actions: [
-          /*IconButton(
+          actions: [
+            /*IconButton(
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => const Sobre()));
             },
             icon: const Icon(Icons.settings),
           ),*/
-          PopupMenuButton(
-            itemBuilder: (context) {
-              return itensMenu.map((String item) {
-                return PopupMenuItem(value: item, child: Text(item));
-              }).toList();
-            },
-            onSelected: _escolhaMenuItem,
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-          //meu metodo nao esta funcionando
-          _Lista();
-        ],)
+            PopupMenuButton(
+              itemBuilder: (context) {
+                return itensMenu.map((String item) {
+                  return PopupMenuItem(value: item, child: Text(item));
+                }).toList();
+              },
+              onSelected: _escolhaMenuItem,
+            )
+          ],
         ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection("Postagem")
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasData) {
+                      final snap = snapshot.data!.docs;
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        primary: false,
+                        itemCount: snap.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            height: 70,
+                            width: double.infinity,
+                            margin: const EdgeInsets.only(bottom: 12),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  offset: Offset(2, 2),
+                                  blurRadius: 10,
+                                ),
+                              ],
+                            ),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  margin: const EdgeInsets.only(left: 20),
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    snap[index]['nome'],
+                                    style: const TextStyle(
+                                      color: Colors.black54,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(right: 20),
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    "\$${snap[index]['descricao']}",
+                                    style: TextStyle(
+                                      color: Colors.green.withOpacity(0.7),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const SizedBox();
+                    }
+                  }),
+            ],
+          ),
+        ));
   }
 }
